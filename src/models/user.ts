a6f375d256bb7245b57d50b13ec80dd7d1940e92
EliLePissenlit <e.clement1002@gmail.com>
2024-12-05 11:03:44 +0100

    user.ts

---
+++ src/models/user.ts
@@ -0,0 +1,39 @@
+import mongoose, { Schema, Document, HydratedDocument } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+export interface IUser extends Document {
+    username: string;
+    email: string;
+    password: string;
+    comparePassword(candidatePassword: string): Promise<boolean>;
+}
+
+const userSchema: Schema = new Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+});
+
+// Middleware pour hacher le mot de passe avant de sauvegarder
+userSchema.pre('save', async function (next) {
+    const user = this as unknown as HydratedDocument<IUser>;
+
+    if (!user.isModified('password')) {
+        return next();
+    }
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+        next();
+    } catch (error: any) {
+        next(error as Error);
+    }
+});
+
+// Méthode pour comparer les mots de passe
+userSchema.methods.comparePassword = async function (candidatePassword: string): Promise<boolean> {
+    return bcrypt.compare(candidatePassword, this.password);
+};
+
+export default mongoose.model<IUser>('User', userSchema);
